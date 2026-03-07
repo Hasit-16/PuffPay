3b831a4034f3d76d97f3c140b772a8989f11ffeb
Hasit Solanki <solankihasit@gmail.com>
2026-03-07 23:29:05 +0530

    fix: notification enable button

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,4 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  customWorkerSrc: "worker", // Points to the /worker directory, expecting index.ts or index.js
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
 
 const nextConfig: NextConfig = {
   turbopack: {},
@@ -41,4 +29,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
